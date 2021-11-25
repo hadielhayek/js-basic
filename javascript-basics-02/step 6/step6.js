228174446483e89cb8a92fc08e1c1cbf2a1e71c6
hadi <hadi-hayek-20@hotmail.com>
2021-11-25 20:04:41 +0200

    step6

---
+++ javascript-basics-02/step 6/step6.js
@@ -0,0 +1,27 @@
+document.querySelector("#image1").addEventListener("mouseover",change);
+document.querySelector("#image2").addEventListener("mouseover",change);
+document.querySelector("#image3").addEventListener("mouseover",change);
+document.querySelector("#image4").addEventListener("mouseover",change);
+document.querySelector("#image5").addEventListener("mouseover",change);
+function change(){
+    document.querySelector("#image1").src="images/image1_2.jpg";
+    document.querySelector("#image2").src="images/image2_2.jpg";
+    document.querySelector("#image3").src="images/image3_2.jpg";
+    document.querySelector("#image4").src="images/image4_2.jpg";
+    document.querySelector("#image5").src="images/image5_2.jpg";}
+
+    document.querySelector("#image1").addEventListener("mouseover",change);
+    document.querySelector("#image2").addEventListener("mouseover",change);
+    document.querySelector("#image3").addEventListener("mouseover",change);
+    document.querySelector("#image4").addEventListener("mouseover",change);
+    document.querySelector("#image5").addEventListener("mouseover",change);
+
+    function change2(){
+        document.querySelector("#image1").src="images/image1.jpg";
+        document.querySelector("#image2").src="images/image2.jpg";
+        document.querySelector("#image3").src="images/image3.jpg";
+        document.querySelector("#image4").src="images/image4.jpg";
+        document.querySelector("#image5").src="images/image5.jpg";}
+
+        
+    
