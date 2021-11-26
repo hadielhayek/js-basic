0e4fe9b577d56ac246a9f5cd5eade60356172d72
hadi <hadi-hayek-20@hotmail.com>
2021-11-26 23:03:50 +0200

    step2

---
+++ javascript-basics-03/step 2/step2.js
@@ -0,0 +1,12 @@
+var t=document.getElementById("texte");
+function view(a){
+        if(a==1){
+            t.style.display="block";
+           
+        }
+        else{
+            t.style.display="none";
+          
+        }
+    
+    }
