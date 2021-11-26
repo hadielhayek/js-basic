2750a7b3db106bf4540868d6f4865acdec080c81
hadi <hadi-hayek-20@hotmail.com>
2021-11-26 23:48:49 +0200

    step3

--- javascript-basics-03/step 2/step2.js
+++ javascript-basics-03/step 2/step2.js
@@ -1,12 +1,10 @@
 var t=document.getElementById("texte");
 function view(a){
-        if(a==1){
-            t.style.display="block";
-           
-        }
-        else{
-            t.style.display="none";
-          
-        }
-    
+    if (a==1) {
+        t.style.display="block"
+        
     }
+    else{
+        t.style.display="none"
+    }
+}
---
+++ javascript-basics-03/step 3/step3.js
@@ -0,0 +1,12 @@
+var t=document.getElementById("text");
+function change(a){
+    if(a==1){
+        t.style.color="green";
+    }
+    else if(a==2){
+        t.style.color="red";
+    }
+    else{
+        t.style.color="blue";
+    }
+}
